c692b1c70461a1b0bddabedfa886b557b50f69fa
Ryan <ryan@avamia.com>
2021-08-08 14:00:25 -0500

    init commit

---
+++ spec/kanrisuru/nginx_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Kanrisuru::Nginx do
+  it "has a version number" do
+    expect(Kanrisuru::Nginx::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
